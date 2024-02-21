5308fba27ee52c2038f0b630f9fb85d1cb47408d
saeed joulaei <saeedjoulaei86@gmail.com>
2024-02-21 13:49:08 +0330

    header component is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>contact app</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div>
+      <h1>Contact App</h1>
+      <p>
+        <a href="https://www.google.com/">Sj-TestApp</a> | Hello World
+      </p>
+    </div>
+  );
+}
+
+export default Header;
