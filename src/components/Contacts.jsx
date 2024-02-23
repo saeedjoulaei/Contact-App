77f0273e3d17ab0544065df676d6bd29b544257c
saeed joulaei <saeedjoulaei86@gmail.com>
2024-02-23 16:07:05 +0330

    contactsList's component and alert section is added to project

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import ContactsList from "./ContactsList";
+
 function Contacts() {
   const [contact, setContact] = useState({
     name: "",
@@ -7,10 +9,32 @@ function Contacts() {
     email: "",
     phone: "",
   });
+  const [contacts, setContacts] = useState([]);
+  const [alert, setAlert] = useState("");
   const changeHandler = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setContact({ ...contact, [name]: value });
+  };
+  const addHandler = () => {
+    if (
+      !contact.name ||
+      !contact.lastname ||
+      !contact.email ||
+      !contact.phone
+    ) {
+      setAlert("please enter valid data");
+      return;
+    }
+    setAlert("");
+    setContacts((contacts) => [...contacts, contact]);
+    setContact({
+      name: "",
+      lastname: "",
+      email: "",
+      phone: "",
+    });
+    console.log(contacts);
   };
   return (
     <div>
@@ -43,8 +67,10 @@ function Contacts() {
           value={contact.phone}
           onChange={changeHandler}
         />
-        <button onClick={() => console.log(contact)}>Add Contact</button>
+        <button onClick={addHandler}>Add Contact</button>
       </div>
+      <div>{alert && <p>{alert}</p>}</div>
+      <ContactsList contacts={contacts} />
     </div>
   );
 }
---
+++ src/components/ContactsList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ContactsList(props) {
+  console.log(props);
+  return <div>ContactsList </div>;
+}
+
+export default ContactsList;
